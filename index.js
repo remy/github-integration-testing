b4e3cd0f041c9bcd3b254fd3ba1c516c44b3361c
Remy Sharp <remy@remysharp.com>
2017-12-08 17:09:29 +0000

    feat: initial handling of PRs

---
+++ index.js
@@ -0,0 +1,19 @@
+// filename: index.js
+function handlePullRequest(repo, ref, data) {
+  // this is where we'll post the git commit status message
+  console.log('got webhook on %s', repo);
+}
+
+function listen(port = 8000, secret = 'a secret code') {
+  const githubhook = require('githubhook');
+  const hook = githubhook({
+    port,
+    secret,
+    path: '/',
+  });
+
+  hook.on('pull_request', handlePullRequest);
+  hook.listen();
+}
+
+listen(process.env.PORT, process.env.SECRET);
