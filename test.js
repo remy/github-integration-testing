4974f32ec9012711eb0b4a5eaf651447ef171450
Remy Sharp <remy@remysharp.com>
2017-12-12 15:12:25 +0000

    only show travis envs

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 
-console.log(process.env);
+console.log(
+  Object.keys(process.env).filter(key => key.startsWith('TRAVIS_')).map(key => ({ [key]: process.env[key] }))
+);
 const npmrc = fs.readFileSync('./.npmrc', 'utf8');
 
 console.log(npmrc, npmrc.length);
